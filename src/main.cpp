75c5fd3e058cad213424d59bd335e6f1378474fc
Hillany <hillanyrf@gmail.com>
2024-08-16 20:35:53 -0400

    Agregue el giroscopio

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,41 @@ Carnet: 14-10937
 */
 
 #include <ESP32Servo.h>
-//#include <DabbleESP32.h>
-//#include <BluetoothSerial.h>
-//#include <WiFi.h>
+#include <Wire.h>
 
 Servo miServo;
-int pinPulsadorH = 32;  
+int pinPulsadorH = 32;  // Cambiar al pin correspondiente
 int pinPulsadorA = 33;
 int pinswPulsador = 12;
+int pinswGiroscopio = 14;
 int estadoPulsadorH = 0;
 int estadoPulsadorA = 0;
 int angulo = 0;
 
+const int MPU_ADDR = 0x68; // Dirección I2C del MPU-6050
+int16_t accelerometer_x, accelerometer_y, accelerometer_z; // Variables para datos crudos del acelerómetro
+int16_t gyro_x, gyro_y, gyro_z; // Variables para datos crudos del giroscopio
+int16_t temperature; // Variable para datos de temperatura
+char tmp_str[7]; // Variable temporal usada en la función de conversión
+
+// Función para convertir int16 a string
+char* convert_int16_to_str(int16_t i) {
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
+
 void setup() {
-  miServo.attach(22);  
+  miServo.attach(19);  // Cambiar al pin correspondiente
   pinMode(pinPulsadorH, INPUT_PULLDOWN);
   pinMode(pinPulsadorA, INPUT_PULLDOWN);
   pinMode(pinswPulsador, INPUT_PULLDOWN);
+  pinMode(pinswGiroscopio, INPUT_PULLDOWN);
+  Serial.begin(115200); // Cambiado a 115200 para una mejor velocidad de comunicación
+  Wire.begin(); // Inicializa la comunicación I2C
+  Wire.beginTransmission(MPU_ADDR); 
+  Wire.write(0x6B); // Registro PWR_MGMT_1
+  Wire.write(0); // Configura a cero para despertar el MPU-6050
+  Wire.endTransmission(true);
 }
 
 void loop() {
@@ -50,12 +68,50 @@ void loop() {
       miServo.write(angulo);
       delay(15); 
     }
-
   }
 
-  if(digitalRead(pinswPulsador) == LOW){
+  if(digitalRead(pinswGiroscopio) == HIGH){
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B); // Comienza con el registro 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false); 
+  Wire.requestFrom(MPU_ADDR, 14, true); // Solicita un total de 14 registros
+  
+  // Leer datos
+  accelerometer_x = Wire.read() << 8 | Wire.read();
+  accelerometer_y = Wire.read() << 8 | Wire.read();
+  accelerometer_z = Wire.read() << 8 | Wire.read();
+  temperature = Wire.read() << 8 | Wire.read();
+  gyro_x = Wire.read() << 8 | Wire.read();
+  gyro_y = Wire.read() << 8 | Wire.read();
+  gyro_z = Wire.read() << 8 | Wire.read();
+  
+  // Imprimir datos
+  Serial.print(convert_int16_to_str(accelerometer_x));Serial.print(","); 
+  Serial.print(convert_int16_to_str(accelerometer_y));Serial.print(","); 
+  Serial.print(convert_int16_to_str(accelerometer_z));
+  
+  //Serial.print(convert_int16_to_str(gyro_z)); Serial.print(",");
+  //Serial.print(convert_int16_to_str(gyro_y)); Serial.print(",");
+  //Serial.print(convert_int16_to_str(gyro_x));
+
+  //Serial.print(accelerometer_x);Serial.print(","); 
+  //Serial.print(accelerometer_y);Serial.print(","); 
+  //Serial.print(accelerometer_z);
+
+  Serial.println();
+
+  if(accelerometer_x > 10000 && accelerometer_x < 25000){
+    int angulop = map(accelerometer_x, 10000, 25000, 0, 180); // Escalar el valor leido a un rango de 0 a 180
+    miServo.write(angulop); // Enviar el valor escalado al servo
+    //Serial.print("angulo: ");
+    //Serial.println(angulop);
+  }
+  
+  delay(20);
+  }
+
+  if(digitalRead(pinswPulsador) == LOW && digitalRead(pinswGiroscopio) == LOW ){
     miServo.write(0);
     delay(15);
   }
-
 }
